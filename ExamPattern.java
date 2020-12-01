235f6b3fc624881c2cfe37e682429f39ebfa1cde
manikumarmandapaka <75292936+manikumarmandapaka@users.noreply.github.com>
2020-12-01 10:30:26 +0530

    Update ExamPattern.java

--- ExamPattern.java
+++ ExamPattern.java
@@ -2,10 +2,23 @@ public class ExamPattern
 {
 public static void main(String[] args) {
          int i, j, k = 1;
-         for (i = 1; i <= 5; i++) {
-             for (j = 1; j< i + 1; j++) {
-                 System.out.print(k++ + " ");
+         int space=5;
+         for (i = 1; i <= 5; i++) 
+         {
+                  space--;
+                  k++;
+                 
+             for (j = 1; j<=space; j++) 
+             {
+                 System.out.print(" ");
              }
+             for (x = 1; x<=k; x++) 
+             {
+                 System.out.print(k);
+                      k++;
+                              
+             }     
+                  
    
              System.out.println();
          }
